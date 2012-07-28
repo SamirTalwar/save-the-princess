cbf8bcf1f9ff1dc001a147fefad7bc89e1da21bb
Samir Talwar <samir@noodlesandwich.com>
2012-07-28 19:08:52 +0100

    First test for Character.

---
+++ spec/character_spec.js
@@ -0,0 +1,23 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+    nodeRequire: require
+});
+
+requirejs(['../lib/character'], function(Character) {
+    describe(Character, function() {
+        it('draws itself onto the layer provided', function() {
+            var layer = createSpyObj('layer', ['Sprite']);
+
+            new Character(layer, 'red', 8, 3);
+
+            expect(layer.Sprite).toHaveBeenCalledWith(false, {
+                color: 'red',
+                x: 400,
+                y: 150,
+                w: 50,
+                h: 50
+            });
+        });
+    });
+});
