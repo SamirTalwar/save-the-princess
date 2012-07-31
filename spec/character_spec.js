429b87dfb17acdbe2252809a4232cea18dca7322
Samir Talwar <samir@noodlesandwich.com>
2012-07-31 17:24:46 +0100

    Stop after moving a square.

--- spec/character_spec.js
+++ spec/character_spec.js
@@ -11,13 +11,6 @@ require(['lib/character'], function(Character) {
         });
 
         it('draws itself onto the layer provided', function() {
-            var sprite = {
-                x: 400,
-                y: 150
-            };
-
-            layer.Sprite.andReturn(sprite);
-
             new Character(layer, 'red', 8, 3);
 
             expect(layer.Sprite).toHaveBeenCalledWith(false, {
@@ -32,9 +25,7 @@ require(['lib/character'], function(Character) {
         it('moves one square per quarter second', function() {
             var sprite = {
                     update: jasmine.createSpy(),
-                    applyVelocity: jasmine.createSpy(),
-                    x: 200,
-                    y: 250
+                    applyVelocity: jasmine.createSpy()
                 },
                 character;
 
@@ -51,6 +42,33 @@ require(['lib/character'], function(Character) {
                 expect(sprite.applyVelocity.calls.length).toEqual(i + 1);
                 expect(sprite.update).toHaveBeenCalled();
             });
+        });
+
+        it('stops after moving one square', function() {
+            var sprite = {
+                    update: jasmine.createSpy(),
+                    applyVelocity: jasmine.createSpy(),
+                    x: 300,
+                    y: 100
+                },
+                character;
+
+            layer.Sprite.andReturn(sprite);
+
+            sprite.applyVelocity.andCallFake(function() {
+                sprite.x += sprite.xv;
+                sprite.y += sprite.yv;
+            });
+
+            character = new Character(layer, 'green', 6, 2);
+
+            character.move(1, 0);
+
+            _(_.range(framesToMove)).each(function() { character.update(); });
+
+            character.update();
+            expect(sprite.xv).toEqual(0);
+            expect(sprite.yv).toEqual(0);
         });
     });
 });
